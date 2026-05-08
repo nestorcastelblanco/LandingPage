f6c26a96cd335b091467be3f3d18f3e286ebcf1b
Nestor Castelblanco <214186561+ncastelblanco29@users.noreply.github.com>
2026-05-08 10:03:09 -0500

    Changes

---
+++ public/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVAhlXGHMSPB9mrWTQqsZdi3GsfqrwtkA",
+  authDomain: "novacast-3c1e4.firebaseapp.com",
+  projectId: "novacast-3c1e4",
+  storageBucket: "novacast-3c1e4.firebasestorage.app",
+  messagingSenderId: "69015523681",
+  appId: "1:69015523681:web:1d0240ce94dfd442853520"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
